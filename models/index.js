a20d5a2c54ec01111135b12dc58e8c027e12d2b8
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-21 22:47:35 -0500

    added product routes

--- models/index.js
+++ models/index.js
@@ -17,14 +17,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'product_tagged',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'product_tagged',
   foreignKey: 'tag_id'
 });
 
